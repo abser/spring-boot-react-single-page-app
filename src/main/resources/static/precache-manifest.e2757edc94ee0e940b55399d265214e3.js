9ff694de752fe5ae4fc61a64309baa1cb94d4fb3
Nurul Abser <nurul.abser@income.com.sg>
2019-05-21 15:57:50 +0800

    Project cleanup

---
+++ src/main/resources/static/precache-manifest.e2757edc94ee0e940b55399d265214e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51fa035bae8bfe1f2fbb",
+    "url": "/static/js/main.51fa035b.chunk.js"
+  },
+  {
+    "revision": "0f91abebe720069110e5",
+    "url": "/static/js/1.0f91abeb.chunk.js"
+  },
+  {
+    "revision": "51fa035bae8bfe1f2fbb",
+    "url": "/static/css/main.97757116.chunk.css"
+  },
+  {
+    "revision": "0f91abebe720069110e5",
+    "url": "/static/css/1.d6f99e6f.chunk.css"
+  },
+  {
+    "revision": "7920aebb629af7f6a77b429f16e43987",
+    "url": "/index.html"
+  }
+];
